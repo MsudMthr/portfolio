4ac1e8e0a1cf901e961774b8592720519d9d9b2e
masoud <masoudmotahari1380@gmail.com>
2022-04-20 07:30:59 -0700

    add contact links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,20 +8,20 @@ import linkedin from "../asset/icons/linkedin-svgrepo-com.svg";
 const Contact = () => {
   return (
     <div className="h-screen bg-slate-100 center">
-      <div className="center h-full w-1/2 flex-wrap gap-10 md:gap-16 max-w-xl ">
-        <a href="/" className="contact">
+      <div className="center md:h-1/2 w-1/2 flex-wrap sm:gap-10 md:gap-14 max-w-xl ">
+        <a href="https://linkedin.com/in/msudmthr" className="contact">
           <img src={linkedin} alt="linkedin" className="h-10 rounded-lg" />
           <span className="font-bold text-blue-600">linkedin</span>
         </a>
-        <a href="/" className="contact">
+        <a href="https://github.com/MsudMthr" className="contact">
           <img src={github} alt="github" className="h-10 rounded-lg" />
           <span className="font-bold ">github</span>
         </a>
-        <a href="/" className="contact">
+        <a href="https://instagram.com/msud.mthr" className="contact">
           <img src={instagram} alt="instagram" className="h-10 rounded-lg" />
           <span className="font-bold text-pink-700">instagram</span>
         </a>
-        <a href="/" className="contact">
+        <a href="skype:Masoud Motahari" className="contact">
           <img src={skype} alt="skype" className="h-10 rounded-lg" />
           <span className="font-bold text-sky-400">skype</span>
         </a>
